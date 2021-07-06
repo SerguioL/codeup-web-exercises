96caa4d86cc6cb7426b0aaff21904505f4b50be9
SerguioL <serguio.l.landa@gmail.com>
2021-07-06 12:28:57 -0500

    external.js first push

---
+++ js/external.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// alert
+
+// /alert("Hi, Im a JavaScript alert")
+
+// confirm
+
+//  var userDecision = confirm("are you sureyou don't want to buy stuff on sale!");
+// console.log(userDecision);
+
+// if(userDecision === true){
+//  alert("wise choice!");
+// } else{
+//  alert("Oh no, your loss!")
+// }
+
+// prompt
+
+// var currentSpeedInMph = 65;
+// var plannedSpeedInMph = prompt("How fast do you plan to go I mph");
+// var distanceLeftInMiles = prompt("How may miles away are you from your destination");
+// var timeToDestinationAtCurrentSpeed = (distanceLeftInMiles/currentSpeedInMph) * 60;
+// console.log(timeToDestinationAtCurrentSpeed);
+// var timeToDestinationAtPlannedSpeed =(distanceLeftInMiles/plannedSpeedInMph) * 60;
+// var timeGoin = timeToDestinationAtCurrentSpeed - timeToDestinationAtPlannedSpeed;
+// var minutesGoined = Math.floor(timeGoin);
+// var seconds = Math.round((timeGoin - minutesGoined) * 60);
+// console.log("If you drive at " + plannedSpeedInMph + " mph,you will arrive " + minutesGoined + " minutes and " + seconds + " seconds earlier. Is it worth it?")
+
+console.log("Hello from external JavaScript");
