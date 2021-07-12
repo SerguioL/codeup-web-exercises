658865f0fc53f07448f6c5e7e1004555fddd4dc6
SerguioL <serguio.l.landa@gmail.com>
2021-07-12 10:39:42 -0500

    first push on function_drill.js finished isOdd and is Even function

---
+++ js/function_drills.js
@@ -0,0 +1,19 @@
+function isOdd(number){
+    if(number % 2 === 0) {
+        return "is not odd";
+    } else {
+        return "is odd";
+    }
+}
+
+function isEven(number){
+    if(number % 2 === 0){
+        return "is even";
+    } else{
+        return "is not even";
+    }
+}
+
+function identity(input){
+    return input;
+}
