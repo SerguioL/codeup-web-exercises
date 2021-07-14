71f273e9620287896d5707f2de08ce4f74919d20
SerguioL <serguio.l.landa@gmail.com>
2021-07-14 12:25:31 -0500

    finished isFive, addFive isMuptipleOfFive, isThree, isMultipleOfThree, isMultipleOfThreeAndFive,and isMultipleOf

--- js/function_drills.js
+++ js/function_drills.js
@@ -17,3 +17,48 @@ function isEven(number){
 function identity(input){
     return input;
 }
+
+function isFive(input){
+    return parseFloat(input) === 5;
+}
+
+function addFive(input){
+    return parseFloat(input) + 5;
+}
+
+function isMultipleOfFive(input){
+    if(input % 5 === 0){
+        return "Multiple of five";
+    } else {
+        return "Not multiple of five";
+    }
+}
+
+function isThree(input){
+    return parseFloat(input) === 3;
+}
+
+function isMultipleOfThree(input){
+    if(input % 3 === 0){
+        return "Multiple of three";
+    }else{
+        return "Not multiple of three";
+    }
+}
+
+function isMultipleOfThreeAndFive(input){
+    if (input % 3 ===0 || input % 5 === 0){
+        return "Multiple of three or five";
+    } else{
+        return "Not a multiple of three or five";
+    }
+}
+
+function isMultipleOf(target,n){
+    if (target % n === 0){
+        return "Is multiple of target"
+    } else{
+        return "Not multiple of target"
+    }
+}
+
