3ac78473547476144b5e2f3b92b25c591d42798f
SerguioL <serguio.l.landa@gmail.com>
2021-07-27 08:42:37 -0500

    finished skipOddNumber function i need to change parseFloat to parseInt

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,29 +1,27 @@
 
 
 
-function skipOneOddNumber(){
-   while (true){
-       let oddNumber = parseFloat(prompt("Pick a number between 1 and 50, to skip "));
-       if (oddNumber % 2 === 1 && oddNumber < 51 && oddNumber >=1){
-           break;
-       }
-       for (let i =1; i,51; i+=2){
-           if(i === oddNumber){
-               console.log("Skipping this number " + oddNumber);
-               continue;
-           }
-           console.log(i)
-       }
-   }
+function skipOddNumber() {
+    while (true) {
+        var oddNumber = parseInt(prompt("Pick a number between 1 and 50, to skip "));
+        if (oddNumber % 2 === 1 && oddNumber < 51 && oddNumber >= 1) {
+            break;
+        }
+    }
+    for (var i = 1; i < 51; i += 2) {
+        if (i === oddNumber) {
+            console.log("Skipping this number " + oddNumber);
+            continue;
+        }
+        console.log(i)
+    }
 }
 
-//console.log(skipOneOddNumber(oddNumber));
-
-//console.log(skipOneOddNumber(27));
+skipOddNumber();
 
 // while (true){
 //     var oddNumber = parseInt(prompt("Please enter an odd number between 1 and 50. I will skip it."));
-//     if (oddNumber % 2 === 1 && oddNumber < 51 && oddNumber >=1){
+//     if (oddNumber % 2 === 1 && oddNumber < 51 && oddNumber >=1) {
 //         break;
 //     }
 // }
